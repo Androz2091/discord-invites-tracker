ccebe85e1988744eb7a44bea92317b2627c59c29
KonkenBonken <konrad05pettersson@gmail.com>
2023-08-20 18:48:20 +0200

    allow usage of CRLF

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
         'lines-between-class-members': 0,
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': 'error',
+        'linebreak-style': ['error', 'windows'],
         'max-len': 0
     }
 };
